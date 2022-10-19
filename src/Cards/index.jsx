bd6073de0bc4ea2d45c0bc4a9a9e500ab6114da7
Keitey <hkmdev22@gmail.com>
2022-10-19 20:42:55 -0300

    add name of characters

--- src/Cards/index.jsx
+++ src/Cards/index.jsx
@@ -12,32 +12,32 @@ const Card = () => {
 
   return (
     <C.CardContainer>
-        {characters.map((character) => {
-          return (
-            <C.Content key={character.id}>
-              <C.Title>Bob's Burgers character</C.Title>
-              <img src={character.image} alt={character.name} />
-              <C.Title>{character.name}</C.Title>
-              <C.CardContent>
-                <span>
-                  <b>Gender:</b> {character.gender}
-                </span>
-                <span>
-                  <b>Hair:</b> {character.hairColor}
-                </span>
-                <span>
-                  <b>Occupation:</b> {character.occupation}
-                </span>
-                <span>
-                  <b>First Episode:</b> {character.firstEpisode}
-                </span>
-                <span>
-                  <b>Voiced by:</b> {character.voicedBy}
-                </span>
-              </C.CardContent>
-            </C.Content>
-          );
-        })}
+      {characters.map((character) => {
+        return (
+          <C.Content key={character.id}>
+            <C.Title>Bob's Burgers character</C.Title>
+            <img src={character.image} alt={character.name} />
+            <C.Title>Name: {character.name}</C.Title>
+            <C.CardContent>
+              <span>
+                <b>Gender:</b> {character.gender}
+              </span>
+              <span>
+                <b>Hair:</b> {character.hairColor}
+              </span>
+              <span>
+                <b>Occupation:</b> {character.occupation}
+              </span>
+              <span>
+                <b>First Episode:</b> {character.firstEpisode}
+              </span>
+              <span>
+                <b>Voiced by:</b> {character.voicedBy}
+              </span>
+            </C.CardContent>
+          </C.Content>
+        );
+      })}
     </C.CardContainer>
   );
 };
